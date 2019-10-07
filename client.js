298bb297efc684317cd7c864d0475dbb5b53b529
Patrick Baker <bakerpj1992@gmail.com>
2019-10-07 14:06:52 -0500

    delete button deletes container from array

--- client.js
+++ client.js
@@ -5,22 +5,37 @@ let containerArray = [];
 
 function readyNow() {
     console.log("in jquery");
-    $("#generate").on("click", generateContainer);
+    $("#generate").on("click", pushContainerToArray);
     $('body').on('click', ".yellowButton", makeYellow);
-    $('body').on('click', "deleteButton", deleteContainer)
+    $('body').on('click', ".deleteButton", deleteContainer);
 }
 
-function generateContainer () {
-    containerCount++;
-    let container = `<div class="container" id="${containerCount}"><button class="yellowButton button">Yellow</button><button class="deleteButton button">Delete</button></div>`
-    containerArray.push(container);
+function appendContainers () {
     $("#containerDiv").empty();
     containerArray.forEach(function(container) {
-        $("#containerDiv").append(container);
+        $("#containerDiv").append(container.container);
     })
+}
+
+function pushContainerToArray () {
+    containerCount++;
+    let container = `<div class="container" id="${containerCount}"><h3 class="counter">${containerCount}</h3><button class="yellowButton button">Yellow</button><button class="deleteButton button">Delete</button></div>`
+    containerArray.push( {
+        container: container,
+        arrayNumber: `${containerCount}`
+    });
+    appendContainers();
 }
 
 function makeYellow () {
     $(this).parent().css("background-color", "yellow");
+}
+function deleteContainer () {
+    let id = $(this).parent().attr('id');
+    console.log(id);
+    console.log(this);
+    let newArray = containerArray.filter(container => id != container.arrayNumber);
+    containerArray = newArray;
+    appendContainers();
 }
 
