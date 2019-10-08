7e376805d5112c9ba9cec6feed20b42f54363643
Patrick Baker <bakerpj1992@gmail.com>
2019-10-08 06:56:54 -0500

    comment out code for usiong array, and reappending through looping after delete button is pressed. append straight to DOM when Generate button is hit

--- client.js
+++ client.js
@@ -11,11 +11,16 @@ function readyNow() {
 }
 
 // empties container div and loops through array to append all remaining containers in array
-function appendContainers () {
+/* This function is only necessary if I use an array, and push from there. I wanted to do this because
+it seemed more of along the lines of where we are headed, wiping the DOM and pulling the information from the 
+database everytime we alter it, but I couldn't get the yellow color to remain. I think this problem is solvable more
+intuitively once we get to "state" in react. */
+/* function appendContainers () {
     $("#containerDiv").empty();
     containerArray.forEach(function(containerInArray) {
         $("#containerDiv").append(containerInArray.container);
-        console.log(containerInArray.color); 
+        console.log(containerInArray.color);
+       // containerInArray.container.style.backgroundColor = "yellow";
      
      
         //   if (containerInArray.color == "yellow") {
@@ -24,39 +29,45 @@ function appendContainers () {
       // messes up the container appending but Im not sure why
     
     })
-}
+} */
 
 // adds to container counters, pushes new container to array, calls appendContainers()
 function pushContainerToArray () {
     containerCount++;
-    let container = `<div class="container" id="${containerCount}"><h3 class="counter">${containerCount}</h3><button class="yellowButton button">Yellow</button><button class="deleteButton button">Delete</button></div>`
-    containerArray.push( {
+    let container = `<div class="container" id="${containerCount}"><p class="counter">${containerCount}</p><button class="yellowButton button">Yellow</button><button class="deleteButton button">Delete</button></div>`
+    $("#containerDiv").append(container);
+    // only need object if I use array.
+    /*  containerArray.push( {
         container: container,
         arrayNumber: `${containerCount}`,
         color: "red"
-    });
-    appendContainers();
+    }); */
+    //appendContainers();
 }
 
 // makes container yellow, changes color property of array object to yellow
 function makeYellow () {
     $(this).parent().css("background-color", "yellow");
-    let id = $(this).parent().attr('id');
+  // only necessary with objects in array.
+    /*  let id = $(this).parent().attr('id');
     for (container of containerArray) {
         if (id == container.arrayNumber) {
             container.color = "yellow";
         }
-    }
+    } */
 }
 
 // removes matching object of container from array, calls appendContainers() to empty container div and reappend containers
 function deleteContainer () {
-    let id = $(this).parent().attr('id');
+    $(this).parent().remove();
+    console.log($(this).parent());
+    // code for using an array.
+    /* let id = $(this).parent().attr('id');
     console.log(id);
     console.log(this);
     let newArray = containerArray.filter(container => id != container.arrayNumber);
     containerArray = newArray;
-    appendContainers();
+    appendContainers(); */
 }
 
 // when deleting a container, all divs turn back to red, I need to find a way to set a property that stays with that container when the array is re pushed.
